25f6a485287ebbd3142a93e91ba4a2bba8a44842
Huzaifa <huzaifanaseer084@gmail.com>
2025-12-03 14:57:59 +0500

    first commit

---
+++ src/main/java/org/devaxiom/safedocs/SafeDocsApplication.java
@@ -0,0 +1,13 @@
+package org.devaxiom.safedocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/devaxiom/safedocs/SafeDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.devaxiom.safedocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
